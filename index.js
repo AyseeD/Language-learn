4ba41e71bcc3332daaa755808d59849da06e6a5a
AyseeD <ayse.delen@yandex.com>
2025-11-18 14:07:40 +0300

    change html files

--- index.js
+++ index.js
@@ -6,37 +6,33 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import FormData from "form-data";
 
-
 const app = express();
 const port = 3000;
 
-// Fix for __dirname in ES module scope
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "20mb" }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req,res)=>{
-    res.render("main.ejs");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "main.html"));
 });
 
-app.get("/login", (req, res)=>{
-    res.render("login.ejs");
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
-app.get("/enter", (req,res)=>{
-    res.render("hub.ejs");
-})
+app.get("/enter", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "hub.html"));
+});
 
-app.get("/japan", (req,res)=>{
-    res.render("japan.ejs");
-})
+app.get("/japan", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "japan.html"));
+});
 
-// POST endpoint to receive the drawing
 app.post("/submit-drawing", async (req, res) => {
   try {
     const imgData = req.body.image;
@@ -46,9 +42,9 @@ app.post("/submit-drawing", async (req, res) => {
 
     console.log("Received image data, length:", imgData.length);
 
-    // Send base64 directly to Flask (bypass file upload issues)
+    // Send base64 directly to Flask
     const response = await axios.post("http://localhost:5001/predict-base64", {
-      image: imgData
+      image: imgData,
     });
 
     console.log("Received prediction:", response.data);
@@ -56,13 +52,12 @@ app.post("/submit-drawing", async (req, res) => {
     res.json({
       prediction: response.data,
     });
-
   } catch (err) {
     console.error("Error in /submit-drawing:", err);
     res.status(500).send("Server error predicting image");
   }
 });
 
-app.listen(port, ()=>{
-    console.log(`Listening to port: ${port}`);
+app.listen(port, () => {
+  console.log(`Listening to port: ${port}`);
 });
